5d0029fe07cbba817aa2aa6c7d76a17b21ced0de
Lilii <lilii-ann.older@voco.ee>
2022-11-04 17:34:05 +0200

    Added a book to UI and LS

---
+++ app.js
@@ -0,0 +1,18 @@
+ui = new UI();
+ls = new LS();
+
+const submitButton = document.querySelector("#submit-book");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputISBN = document.querySelector("#isbn");
+const table = document.querySelector("table");
+
+submitButton.addEventListener("click", addBook);
+
+function addBook(e){
+    const book = new Book(inputTitle.value, inputAuthor.value, inputISBN.value);
+    ui.addBook(book.title, book.author, book.isbn);
+    ls.addBook(book);
+    book.addedToUI();
+    e.preventDefault();
+}
